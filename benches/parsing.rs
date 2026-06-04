a370096f290dc6a3c0d153ee1835003dbce93568
Ruben De Smet <ruben.de.smet@rubdos.be>
2026-06-04 10:38:07 +0200

    chore: bump criterion

--- benches/parsing.rs
+++ benches/parsing.rs
@@ -1,4 +1,5 @@
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use std::hint::black_box;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let cases = [
